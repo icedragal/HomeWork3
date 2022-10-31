247882879628d89d2c5d46cb030dbcb009e25bd1
Icedragal <icedragal@gmail.com>
2022-10-31 20:52:07 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Task 1
+        int myAge = 20;
+        System.out.println(myAge);
+        byte num = 125;
+        System.out.println(num);
+        short earthRadius = 6371;
+        System.out.println(earthRadius);
+        long myFinance = -2147483648;
+        System.out.println(myFinance);
+        float myWeight = 65.5F;
+        System.out.println(myWeight);
+        double doubleExample = 1.123124124122312312;
+        System.out.println(doubleExample);
 
 
     }
